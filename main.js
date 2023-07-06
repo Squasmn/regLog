a48aaaa45140f6786f709b652c00bc7e48bd1c77
Squasmn <Squasmn.withem@gmail.com>
2023-07-06 14:11:14 +0200

    Registration fixed. Create userdata as obj and store in .json array

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 // ESM syntax is supported.
 export {};
-const fs = require("fs");
+import fs from "fs";
+
 // Group Project registration and login functionality
 
 // Erstellen einer login und registrier Webpage
 
-// Arbeitsteilung:
-
-// Team Member 1: Backend Development
-
-// **Team Member 1: Backend Development**
-// 1. Set up the project structure: Create the directory structure for the project and initialize a new Node.js project using npm.
-// 2. Design the data structure: Define the structure of the user data, including username, password, and additional profile information, using JavaScript objects and arrays.
 // 3. Implement user registration functionality:
 //    - Create a function that takes user registration data as input and performs the necessary validations.
 //    - Check for requirements like username uniqueness, password complexity, etc.
@@ -22,18 +16,12 @@ const fs = require("fs");
 // 5. Implement user profile management functionality:
 //    - Create functions to handle profile retrieval and update requests.
 //    - Read and update the user's profile information in the separate JSON file using the Node.js File System (fs) module.
-// 6. Handle error cases:
-//    - Implement proper error handling for scenarios like invalid inputs, duplicate usernames during registration, incorrect login credentials, etc.
-//    - Define appropriate error messages and handle them gracefully in your code.
-// 7. Test the backend functionality:
-//    - Write unit tests to verify the registration, login, and profile management functionalities.
-//    - Test different scenarios and edge cases to ensure the proper functioning of the backend code.
 
 // To implement these adjustments, you can use the Node.js File System (fs) module to read from and write to a separate JSON file to store the user data securely on the server-side. The JSON file can be created, updated, and accessed using functions provided by the fs module, such as `fs.readFile`, `fs.writeFile`, `fs.readFileSync`, `fs.writeFileSync`, etc.
 
 // Remember to handle file read/write operations asynchronously and handle errors appropriately. Additionally, ensure proper file paths and file permissions are set when accessing the JSON file.
 
-class userData {
+class UserData {
   constructor(username, password, email) {
     this.username = username;
     this.password = password;
@@ -41,15 +29,22 @@ class userData {
   }
 }
 
-function storeUserData(userData) {
-  // Read the existing data from the JSON file (if any)
+function addUser(userData) {
+  // Check if the file exists before reading
+  if (!fs.existsSync("userdata.json")) {
+    console.error("userdata.json does not exist.");
+    return;
+  }
+
+  // Read the existing data from the JSON file
   let existingData = [];
+  let jsonData;
   try {
-    const jsonData = fs.readFileSync("userdata.json", "utf-8");
+    jsonData = fs.readFileSync("userdata.json", "utf-8");
     existingData = JSON.parse(jsonData);
   } catch (error) {
-    // Handle the case where the file doesn't exist yet or cannot be read
     console.error("Error reading userdata.json:", error);
+    return;
   }
 
   // Check for duplicate registrations
@@ -57,31 +52,26 @@ function storeUserData(userData) {
     (user) =>
       user.username === userData.username || user.email === userData.email
   );
+
   if (isDuplicate) {
     console.error("User with the same username or email already exists.");
     return;
   }
 
-  // Add the new user data to the existing data
+  // Add the new user data to the existing array
   existingData.push(userData);
 
   // Convert the updated data to JSON string
   const updatedData = JSON.stringify(existingData, null, 2);
 
   // Write the updated data back to the JSON file
-  fs.writeFile("userdata.json", updatedData, "utf-8", (error) => {
-    if (error) {
-      // Handle the case where the file cannot be written
-      console.error("Error writing userdata.json:", error);
-    } else {
-      console.log("User data stored successfully.");
-    }
-  });
-}
-const user1 = new userData("John", "password123", "john@example.com");
-const user2 = new userData("Jane", "password456", "jane@example.com");
-// Add more instances as needed
+  fs.writeFileSync("userdata.json", updatedData, "utf-8");
 
-storeUserData(user1);
-storeUserData(user2);
-// Call the function for additional instances
+  console.log("User added successfully.");
+}
+
+const newUser = new UserData("Alice", "password789", "alice@example.com");
+addUser(newUser);
+
+const newUser2 = new UserData("Bob", "password123", "bob@example.com");
+addUser(newUser2);
